e6486f1d1eaef8d1733618a0bad8d91a4747d31c
yisrose35 <yisrose35@gmail.com>
2025-11-30 23:16:36 -0500

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -121,6 +121,9 @@ function renderUsageTable(divName) {
             });
         });
     });
+        // DEBUG: what the analytics Usage Manager is seeing
+    window.debugAnalyticsRawCounts = rawCounts;
+
 
     let html = `<table class="report-table"><thead><tr><th style="text-align:left;">Bunk</th><th style="text-align:left;">Activity</th><th>History Count</th><th>Manual Adj (+/-)</th><th>Effective Total</th><th>Max Limit</th></tr></thead><tbody>`;
 
@@ -172,6 +175,8 @@ function renderUsageTable(divName) {
         };
     });
 }
+        
+
 
 // --- AVAILABILITY GRID (Unchanged but required for file completeness) ---
 function renderFieldAvailabilityGrid() {
