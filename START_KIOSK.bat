0e3a304a93c8bb9de652d463bbb38a9bc668738c
yisrose35 <yisrose35@gmail.com>
2026-02-11 01:01:29 -0500

    Update START_KIOSK.bat

--- START_KIOSK.bat
+++ START_KIOSK.bat
@@ -1,2 +1,8 @@
 @echo off
-start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --kiosk --app="file:///%~dp0flow.html"
+REM ================================================================
+REM  CAMPISTRY KIOSK MODE
+REM  
+REM  Exit: Use the Exit Demo button (password protected)
+REM  Emergency exit: Ctrl+Alt+Delete then Task Manager then End Chrome
+REM ================================================================
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --kiosk --disable-pinch --overscroll-history-navigation=0 --user-data-dir="%~dp0chrome_kiosk_profile" --app="file:///%~dp0flow.html"
