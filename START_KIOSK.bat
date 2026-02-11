fa06fbabfd27698618321d872231640fcd1323d8
yisrose35 <yisrose35@gmail.com>
2026-02-11 00:43:44 -0500

    Update START_KIOSK.bat

--- START_KIOSK.bat
+++ START_KIOSK.bat
@@ -1,2 +1,2 @@
 @echo off
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk --app="file:///%~dp0flow.html"
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --kiosk --app="file:///%~dp0flow.html"
