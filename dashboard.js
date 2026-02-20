efc4599932d68f8211a4c359b4de87d8d515aec9
yisrose35 <yisrose35@gmail.com>
2026-02-20 15:11:06 -0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -292,7 +292,7 @@
         userName = null;
         campName = null;
         // Don't cache uncertain state
-        // localStorage.setItem('campistry_user_id', currentUser.id);    }
+        // localStorage.setItem('campistry_user_id', currentUser.id);     }
 
     // ========================================
     // ★★★ v2.4: CACHE RBAC CONTEXT FOR OTHER PAGES ★★★
