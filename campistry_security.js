227e9f580f126f25adfabc4005404d0713be4d30
yisrose35 <yisrose35@gmail.com>
2026-03-24 23:25:22 -0400

    Update campistry_security.js

--- campistry_security.js
+++ campistry_security.js
@@ -862,11 +862,10 @@
         const csp = [
             "default-src 'self'",
             "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net",
-            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net",
             "font-src 'self' https://fonts.gstatic.com",
             "img-src 'self' data: blob: https:",
-            "connect-src 'self' https://bzqmhcumuarrbueqttfh.supabase.co wss://bzqmhcumuarrbueqttfh.supabase.co https://fonts.googleapis.com https://fonts.gstatic.com https://cdn.jsdelivr.net",
-            "base-uri 'self'",
+            "connect-src 'self' https://bzqmhcumuarrbueqttfh.supabase.co wss://bzqmhcumuarrbueqttfh.supabase.co https://fonts.googleapis.com https://fonts.gstatic.com https://cdn.jsdelivr.net https://api.openrouteservice.org https://*.tile.openstreetmap.org",            "base-uri 'self'",
             "form-action 'self'"
         ].join('; ');
         
