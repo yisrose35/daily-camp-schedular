3282fb0e2e8aab4d7de81725994d28f1b2991b07
yisrose35 <yisrose35@gmail.com>
2026-03-26 02:58:24 -0400

    Update campistry_go.js

--- campistry_go.js
+++ campistry_go.js
@@ -1600,7 +1600,7 @@
                 }
                 return { tour, dist: tourDist(tour) };
             }
-        }
+        
 
         // Helper: calculate route distance for a set of stops
         function routeDist(busStops) {
